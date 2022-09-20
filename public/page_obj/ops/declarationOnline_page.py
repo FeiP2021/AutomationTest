34aff9988730259b819770df61f5db17c8da723b
FeiP2021 <zhaoyj@huimor.com>
2022-09-20 14:57:45 +0800

    update 更新在线申报C类信息

--- public/page_obj/ops/declarationOnline_page.py
+++ public/page_obj/ops/declarationOnline_page.py
@@ -54,14 +54,31 @@ class declarationOnline(Page):
     declarationOnline_button10 = (By.XPATH, testData.get_elementinfo(10))
     # 选常用
     declarationOnline_button11 = (By.XPATH, testData.get_elementinfo(11))
-    # 点预览
+    # 选常用
     declarationOnline_button12 = (By.XPATH, testData.get_elementinfo(12))
-    # 点击我同意
+    # 填1
     declarationOnline_button13 = (By.XPATH, testData.get_elementinfo(13))
-    # 点击提交
+    # 填2
     declarationOnline_button14 = (By.XPATH, testData.get_elementinfo(14))
-    # 点击确定
+    # 保存
     declarationOnline_button15 = (By.XPATH, testData.get_elementinfo(15))
+    # 点预览
+    declarationOnline_button16 = (By.XPATH, testData.get_elementinfo(16))
+    # 点击我同意
+    declarationOnline_button17 = (By.XPATH, testData.get_elementinfo(17))
+    # 点击提交
+    declarationOnline_button18 = (By.XPATH, testData.get_elementinfo(18))
+    # 点击确定
+    declarationOnline_button19 = (By.XPATH, testData.get_elementinfo(19))
+    # 选1
+    declarationOnline_button20 = (By.XPATH, testData.get_elementinfo(20))
+    # 选2
+    declarationOnline_button21 = (By.XPATH, testData.get_elementinfo(21))
+    # 选3
+    declarationOnline_button22 = (By.XPATH, testData.get_elementinfo(22))
+    # 保存的下一步
+    declarationOnline_button23 = (By.XPATH, testData.get_elementinfo(23))
+
 
         #self.driver.switch_to_window(windows[0])  # 切换回窗口
         #sleep(3)
@@ -84,23 +101,47 @@ class declarationOnline(Page):
         sleep(1)
         self.find_element(*self.declarationOnline_button5).click()
         sleep(1)
-        Select(self.find_element(*self.declarationOnline_button6)).select_by_index(0)
-        sleep(2)
-        Select(self.find_element(*self.declarationOnline_button7)).select_by_index(0)
-        sleep(2)
+        self.find_element(*self.declarationOnline_button6).click()
+        sleep(1)
+        above1 = self.find_element(*self.declarationOnline_button20)
+        ActionChains(self.driver).move_to_element(above1).perform()
+       # sleep(1)
+        self.find_element(*self.declarationOnline_button20).click()
+        self.find_element(*self.declarationOnline_button7).click()
+        sleep(1)
+        above1 = self.find_element(*self.declarationOnline_button21)
+        ActionChains(self.driver).move_to_element(above1).perform()
+       # sleep(1)
+        self.find_element(*self.declarationOnline_button21).click()
         self.find_element(*self.declarationOnline_button8).send_keys('100')
         sleep(1)
-        Select(self.find_element(*self.declarationOnline_button9)).select_by_index(1)
-        sleep(2)
+        self.find_element(*self.declarationOnline_button9).click()
+        sleep(1)
+        above1 = self.find_element(*self.declarationOnline_button22)
+        ActionChains(self.driver).move_to_element(above1).perform()
+       # sleep(1)
+        self.find_element(*self.declarationOnline_button22).click()
         self.find_element(*self.declarationOnline_button10).click()
         sleep(1)
         self.find_element(*self.declarationOnline_button11).click()
         sleep(1)
+        windows=self.driver.window_handles
+        self.driver.switch_to_window(windows[-1])
         self.find_element(*self.declarationOnline_button12).click()
         sleep(1)
-        self.find_element(*self.declarationOnline_button13).click()
+        self.find_element(*self.declarationOnline_button13).send_keys('1')
         sleep(1)
-        self.find_element(*self.declarationOnline_button14).click()
+        self.find_element(*self.declarationOnline_button14).send_keys('1')
         sleep(1)
         self.find_element(*self.declarationOnline_button15).click()
+        sleep(1)
+        self.find_element(*self.declarationOnline_button23).click()
+        sleep(1)
+        self.find_element(*self.declarationOnline_button16).click()
+        sleep(1)
+        self.find_element(*self.declarationOnline_button17).click()
+        sleep(1)
+        self.find_element(*self.declarationOnline_button18).click()
+        sleep(1)
+        self.find_element(*self.declarationOnline_button19).click()
         sleep(1)
